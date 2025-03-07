aa536d5082cb316c2165060800cad6429c8c7f17
André Santos <andreluissilsan@hotmail.com>
2025-03-06 22:35:41 -0300

    feature/home - homepage initial ideia

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,11 +1,30 @@
-import { Container, Paper } from "@mui/material";
+import { Box, Container, Fade, Paper, Typography } from "@mui/material";
 
 import "./Style.css";
 
+import image from "../../images/andre.jpeg";
+
 export default function Home() {
     return (
-        <main className="home-container">
-            Hello world
-        </main>
+        <Fade in={true}>
+            <nav className="home-container">
+                <Box className="left-side">
+                    <div style={{ display: "flex", justifySelf: "center" }}>
+                        <img src={image} className="img-style"/>
+                    </div>
+
+                    <Typography color="var(--main-text)" textAlign={"center"} fontWeight={"bold"}>
+                        André Santos <br/>
+                        23 years <br/>
+                        Software Developer <br/>
+                        Computer Scientist
+                    </Typography>
+                </Box>
+
+                <Box className="right-side">
+                    a
+                </Box>
+            </nav>
+        </Fade>
     );
 }

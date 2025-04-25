103133cda8651e9e07dbb128433aeed27a4b6383
andrelssn <andreluissilsan@hotmail.com>
2025-04-24 21:01:46 -0300

    projects - components

--- src/Views/Projects/Projects.js
+++ src/Views/Projects/Projects.js
@@ -1,41 +1,17 @@
 import React from "react";
-import { Fade, Typography, Button } from "@mui/material";
+import { Fade } from "@mui/material";
 
+// Style
 import "./Style.css";
 
-import webicon from "../../images/webicon.png";
+// Components
+import Portfolio from "../../Components/Projects/Portfolio";
 
 export default function Projects() {
-    const [openLog, setOpenLog]     = React.useState(false);
-    const [openAbout, setOpenAbout] = React.useState(false);
-
     return (
         <Fade in={true}>
             <nav className="projects-container">
-                <section className="projects-section">
-                    <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
-                        <img src={webicon} style={{ width: 30, marginRight: 5 }}/>
-                        <Typography fontSize={26} color="var(--main-text)">
-                            Portfolio André Santos
-                        </Typography>
-
-                        <div style={{ position: "absolute", right: 0 }}>
-                            <Button onClick={() => setOpenLog(true)}>
-                                Log
-                            </Button>
-
-                            <Button onClick={() => setOpenAbout(true)}>
-                                Sobre
-                            </Button>
-                        </div>
-                   </div>
-
-                    <span style={{ color: "#cecece" }}>versão 1.0.0</span>
-
-                    <div className="job-information-box">
-                        
-                    </div>
-                </section>
+                <Portfolio/>
             </nav>
         </Fade>
     );

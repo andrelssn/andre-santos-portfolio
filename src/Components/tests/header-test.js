a511ddedaf932b322908053d25ca1df93a39d842
André Santos <andreluissilsan@hotmail.com>
2025-08-02 12:23:47 -0300

    initial commit

---
+++ src/Components/tests/header-test.js
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react"
+import Header from "Layout/Header/Header"
+
+describe("HeaderTest", () => {
+    it("should render correctly", () => {
+        render(<Header/>)
+
+        expect() 
+    })
+})
